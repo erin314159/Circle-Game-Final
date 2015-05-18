57feefdec55afe6115b6efd0cececdb67c91faf7
Alexandra Ham <alexandraham@sfhs.com>
2015-05-17 20:34:28 -0700

    comment code

--- GameViewer.java
+++ GameViewer.java
@@ -14,38 +14,38 @@ import java.awt.geom.Rectangle2D;
 public class GameViewer extends JComponent //draws all the components for the frame
 {
     private ArrayList<Circle> circles = new ArrayList<Circle>();//declaring and initializing an ArrayList of circles
-    String s = "";
-    String score = "";
-    public GameViewer() 
+    static String s = "";//statics variables for showing score, can be acccessed in the Starter class 
+    static String score = "";
+    public GameViewer() //constructor for gameviewer class 
     {
     }
     
     
-    public void paintComponent(Graphics g) 
+    public void paintComponent(Graphics g) //method in JComponent class that paints all the elements in the circles ArrayList, as well as drawing the strings for the score at the end
     {
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.BLACK);
-        g2.fill(new Rectangle2D.Double(0,0,Starter.framewidth, Starter.frameheight));
+        g2.fill(new Rectangle2D.Double(0,0,Starter.framewidth, Starter.frameheight));//draws a black rectangle as the background 
         for(Circle circle : circles) circle.paint((Graphics2D)g);
-        g2.setColor(Color.WHITE);
-        g2.setFont(new Font(Font.SERIF,Font.PLAIN, 70));
-        g2.drawString(score, 200, 400);
-        g2.drawString(s, 300, 300);
+        g2.setColor(Color.WHITE);//sets color to white
+        g2.setFont(new Font(Font.SERIF,Font.PLAIN, 70));//sets font to a new font 
+        g2.drawString(score, 200, 400);//draws string game over at coordinates 200, 400 with the font created above
+        g2.drawString(s, 300, 300);//draws string for score
     }
     
-    public void add(Circle circle)
+    public void add(Circle circle)//mutator method that adds a desired circle to the arraylist and repaints the JComponent
     {
-        circles.add(circle);
+        circles.add(circle);//adds circle to arraylist
+        this.repaint();//repaints the game viewer 
+    }
+    
+    public void remove(Circle circle)//mutator method that removes desired circle 
+    {
+        circles.remove(circle);//removes circle from arraylist 
         this.repaint();
     }
     
-    public void remove(Circle circle)
-    {
-        circles.remove(circle);
-        this.repaint();
-    }
-    
-    public ArrayList<Circle> getArrayList()
+    public ArrayList<Circle> getArrayList()//accessor method that returns the circles array list
     {
         return circles;
     }
--- MainCircle.java
+++ MainCircle.java
@@ -10,13 +10,12 @@ import java.awt.event.*;
 import java.awt.Graphics2D;
 import java.awt.Graphics;
 import java.awt.geom.Ellipse2D;
-
 import java.awt.Color;
 public class MainCircle extends Circle
 {
-   public int x;
+   public int x;//instance field variables 
    public int y;
-   public int w;
+   public static int w;//static variable for main circle size, able to access from 
    public Ellipse2D.Double mCircle;
    public MainCircle()
    {
@@ -24,18 +23,18 @@ public class MainCircle extends Circle
        y = Starter.frameheight/2;
        w=20;
     }
-    public void paint(Graphics2D g)
+    public void paint(Graphics2D g)//method that paints the main Circle
     {
         Graphics2D g2 = (Graphics2D) g;
          mCircle = new Ellipse2D.Double(x,y,w,w);
         g2.setColor(Color.WHITE);
         g2.fill(mCircle);
     }
-    public void enlarge()
+    public void enlarge()//mutator method for increasing the width of the circle
     {
-        w=w+1;
+        w=w+1;//adds 1 to w, or the height and width 
     }
-    public Ellipse2D getCircle()
+    public Ellipse2D getCircle()//accessor method to return the Ellipse2D mCircle 
     {
         return mCircle;
     }
